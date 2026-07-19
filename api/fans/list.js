b568764ee7180951f1d3b3f4b9396e1a0a6e0fe1
YCXAZZO <69510673+YCXAZZO@users.noreply.github.com>
2026-07-19 23:29:13 +0800

    Update list.js

--- api/fans/list.js
+++ api/fans/list.js
@@ -1,4 +1,4 @@
-import { get } from '../lib/redis.js';
+import { get } from '../../lib/redis.js';
 
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
